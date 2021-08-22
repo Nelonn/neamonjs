e89cce7a18fe6a98962f58c7db528512fd2a720e
Nelonn <nelonn@list.ru>
2021-08-22 18:17:05 +0300

    Fix

--- bin/neamon.js
+++ bin/neamon.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const pathIsAbsolute = requier('path-is-absolute');
 const loader = require('../lib/loader');
 const commandLine = loader.get('command-line');
 
 const commands = commandLine.parse();
 if (!commands._[0]) return console.error('Provide script path');
 
-const scriptPath = path.join(__dirname, '../..', commands._[0]);
+const scriptPath = pathIsAbsolute(commands._[0]) ? commands._[0] : path.join(__dirname, '../../..', commands._[0]);
 loader.get(scriptPath);
