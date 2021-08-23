9636b264bf6c7daaa6fff6eb35386fc1c801189b
Nelonn <nelonn@list.ru>
2021-08-23 22:14:40 +0300

    Hot fix 0.1.1

--- bin/neamon.js
+++ bin/neamon.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const pathIsAbsolute = require('path-is-absolute');
 const loader = require('../lib/loader');
 const commandLine = loader.get('command-line');
 
 const commands = commandLine.parse();
 if (!commands._[0]) return console.error('Provide script path');
 
-const scriptPath = pathIsAbsolute(commands._[0]) ? commands._[0] : path.join(process.cwd(), commands._[0]);
+const scriptPath = path.isAbsolute(commands._[0]) ? commands._[0] : path.join(process.cwd(), commands._[0]);
 loader.get(scriptPath);
